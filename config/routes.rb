e33574714af6cb257202b70a77cfe37ca5bf931c
akasatan <aka435304@gmail.com>
2021-04-14 14:08:34 +0000

    [add]README

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+  }
   devise_for :admins, only: :sessions, controllers: {
     sessions: 'admins/sessions'
   }
